478cb900044c961ec7190f9276b94b5b3c6eecaa
Yashodhar <61496481+myashodhar@users.noreply.github.com>
2023-10-03 15:17:26 +0530

    Update course.h

--- course.h
+++ course.h
@@ -3,12 +3,12 @@
 #include<iostream>
 #include<iomanip>
 #include<fstream>
-#include<string>
+#include<format>
 
 class course
 {
-    string name;
-    string code;
+    std::string name;
+    std::string code;
     int credit;
 
     public:
@@ -25,12 +25,12 @@ void course::get_course_details(int e)
         fstream r;
         r.open("student.txt",ios::out | ios::app);
 
-        cout<<"\n\nCOURSE NAME  :";
-        cin>>name;
-        cout<<"\n\nCOURSE CODE :";
-        cin>>code;
-        cout<<"\n\nCOURSE CREDIT :";
-        cin>>credit;
+        std::cout<<"\n\nCOURSE NAME  :";
+        std::cin>>name;
+        std::cout<<"\n\nCOURSE CODE :";
+        std::cin>>code;
+        std::cout<<"\n\nCOURSE CREDIT :";
+        std::cin>>credit;
 
         r<<"\n\n"<<name<<"\t"<<code<<"\t"<<credit;
         r.close();
@@ -41,26 +41,26 @@ void course::get_course_details(int e)
         fstream r;
         r.open("faculty.txt",ios::out | ios::app);
 
-        cout<<"\n\nCOURSE NAME  :";
-        cin>>name;
-        cout<<"\n\nCOURSE CODE :";
-        cin>>code;
-        cout<<"\n\nCOURSE CREDIT :";
-        cin>>credit;
+        std::cout<<"\n\nCOURSE NAME  :";
+        std::cin>>name;
+        std::cout<<"\n\nCOURSE CODE :";
+        std::cin>>code;
+        std::cout<<"\n\nCOURSE CREDIT :";
+        std::cin>>credit;
 
         r<<"\n\n"<<name<<"\t"<<code<<"\t"<<credit;
         r.close();
     }
 }
 
-int course::get_credit()
+int course::get_credit() const
 {
     return credit;
 }
 
-void course::print_course_details()
+void course::print_course_details() const
 {
-    cout<<"\nNAME:"<<setw(15)<<name<<"\n\n";
+    std::cout<<std::format("\n NAME:{:>15}{0}\n\n", name);
     cout<<"\nCODE:"<<setw(15)<<code<<"\n\n";
     cout<<"\nCREDIT:"<<setw(15)<<credit<<"\n\n";
 }
